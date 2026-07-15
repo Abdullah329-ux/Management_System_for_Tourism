33634f73d9374e02fca5119513568f0b3695422f
Abdullah Riaz <abdullah12riaz1@gmail.com>
2026-07-15 14:27:39 +0500

    Add files via upload

---
+++ tsm/admin/includes/sidebarmenu.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+	<title>Document</title>
+</head>
+<body>
+<div class="sidebar-menu">
+					<header class="logo1">
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+					</header>
+						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
+										
+									 	<li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Tour Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+												<li id="menu-academico-avaliacoes" ><a href="create-package.php">Create</a></li>
+												<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">Manage</a></li>
+										  </ul>
+										</li>
+
+
+										<li id="menu-academico1" ><a href="#"><i class="fa-solid fa-hotel"></i><span> Hotel Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub1" >
+												<li id="menu-academico-avaliacoes1" ><a href="create-package-hotel.php">Create</a></li>
+												<li id="menu-academico-avaliacoes1" ><a href="manage-packages-hotel.php">Manage</a></li>
+										  </ul>
+										</li>
+
+										
+										<li id="menu-academico1" ><a href="#"><i class="fa-solid fa-utensils"></i><span> Resturant Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub1" >
+												<li id="menu-academico-avaliacoes1" ><a href="create-package-rs.php">Create</a></li>
+												<li id="menu-academico-avaliacoes1" ><a href="manage-packages-rs.php">Manage</a></li>
+										  </ul>
+										</li>
+
+										
+									<li id="menu-academico" ><a href="manage-users.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage Users</span><div class="clearfix"></div></a></li>
+									<li><a href="manage-bookings.php"><i class="fa fa-list" aria-hidden="true"></i>  <span>Manage Booking</span><div class="clearfix"></div></a></li>
+									 <li><a href="manageissues.php"><i class="fa fa-table"></i>  <span>Manage Issues</span><div class="clearfix"></div></a></li>
+									<li><a href="manage-enquires.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Enquiries</span><div class="clearfix"></div></a></li>
+									<li><a href="manage-pages.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Pages</span><div class="clearfix"></div></a></li>
+									<!-- <li><a href="manage-hotels.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>manage Hotels</span><div class="clearfix"></div></a></li> -->
+
+									
+								  </ul>
+								</div>
+							  </div>
+</body>
+</html>
