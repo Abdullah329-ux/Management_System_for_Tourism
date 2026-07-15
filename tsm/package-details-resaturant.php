1cf12936bd51808086d1f01ff73e855dd8c2320a
Abdullah Riaz <abdullah12riaz1@gmail.com>
2026-07-15 13:49:27 +0500

    Add files via upload

---
+++ tsm/package-details-resaturant.php
@@ -0,0 +1,228 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+$pid=intval($_GET['pkgid']);
+
+echo  $pid;
+if(isset($_POST['submit2']))
+{
+$pid=intval($_GET['pkgid']);
+$useremail=$_SESSION['login'];
+$fromdate=$_POST['fromdate'];
+// $todate=$_POST['todate'];
+$comment=$_POST['comment'];
+ $status=0;
+$Resaturant_Package='Resaturant';
+$sql="INSERT INTO tblbooking(RID,UserEmail,fromdate,Comment,status,package_category) VALUES(:pid,:useremail,:fromdate,:comment,:status,:Resaturant_Package)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':pid',$pid,PDO::PARAM_STR);
+$query->bindParam(':useremail',$useremail,PDO::PARAM_STR);
+$query->bindParam(':fromdate',$fromdate,PDO::PARAM_STR);
+// $query->bindParam(':todate',$todate,PDO::PARAM_STR);
+$query->bindParam(':comment',$comment,PDO::PARAM_STR);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->bindParam(':Resaturant_Package',$Resaturant_Package,PDO::PARAM_STR);
+
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Booked Successfully";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TMS | Package Details</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<link rel="stylesheet" href="css/jquery-ui.css" />
+	<script>
+		 new WOW().init();
+	</script>
+<script src="js/jquery-ui.js"></script>
+					<!-- <script>
+						$(function() {
+						$( "#datepicker,#datepicker1" ).datepicker();
+						});
+					</script> -->
+	  <style> 
+		.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+		</style>				
+</head>
+<body>
+<!-- top-header -->
+<?php include('includes/header.php');?>
+<div class="banner-3">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> TMS -Package Details</h1>
+	</div>
+</div>
+<!--- /banner ---->
+<!--- selectroom ---->
+<div class="selectroom">
+	<div class="container">	
+		  <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+<?php 
+$pid=intval($_GET['pkgid']);
+$sql = "SELECT * from restaurant where RID =:pid";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':pid', $pid, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>
+
+<form name="book" method="post">
+		<div class="selectroom_top">
+			<div class="col-md-4 selectroom_left wow fadeInLeft animated" data-wow-delay=".5s">
+				<img src="admin/pacakgeimages/<?php echo htmlentities($result->image);?>" class="img-responsive" alt="">
+			</div>
+			<div class="col-md-8 selectroom_right wow fadeInRight animated" data-wow-delay=".5s">
+				<h2><?php echo htmlentities($result->Name);?></h2>
+				<p class="dow">#PKG-<?php echo htmlentities($result->HOTELID );?></p>
+				<p><b>Package Type :</b> <?php echo htmlentities($result->RID );?></p>
+				<p><b>Package Location :</b> <?php echo htmlentities($result->ADDRESS);?></p>
+					<p><b>Features</b> <?php echo htmlentities($result->Restaurant_Features);?></p>
+
+					<div class="ban-bottom">
+						<div class="bnr-right">
+							<label class="inputLabel">Booking date</label>
+							<!-- <input class="date" id="datepicker" type="text" placeholder="dd-mm-yyyy"  name="fromdate" required=""> -->
+							Booking Date: <input type="date" class="date" id="datepicker" placeholder="dd-mm-yyyy"  name="fromdate" required>
+						</div>
+						<!-- <div class="bnr-right">
+							<label class="inputLabel">To</label>
+ 							End Date: <input type="date" class="date" id="datepicker1" placeholder="dd-mm-yyyy" name="todate" required>
+						</div> -->
+					</div> 
+					<!-- <form id="dateForm">
+    Start Date: <input type="date" id="datepicker" required><br>
+    End Date: <input type="date" id="datepicker1" required><br>
+</form> -->
+						<div class="clearfix"></div>
+
+						
+<!-- <p id="new_date"></p> -->
+
+<!-- <script>
+// Function to calculate the difference in days between two dates
+function calculateDays() {
+    const startDate = new Date(document.getElementById("datepicker").value);
+    const endDate = new Date(document.getElementById("datepicker1").value);
+
+   
+        const timeDifference = endDate.getTime() - startDate.getTime();
+        const dayDifference = timeDifference / (1000 * 3600 * 24);
+       var getwhole_date= document.getElementById("result").innerHTML =(dayDifference+1)*<?php //echo htmlentities($result->hotel_PRICE_IN_PKR);?> + " PKR.";
+
+}
+
+// Event listeners for real-time calculation
+document.getElementById("datepicker").addEventListener("change", calculateDays);
+document.getElementById("datepicker1").addEventListener("change", calculateDays);
+</script> -->
+
+				<div class="grand">
+					<h3 >Rating</h3>
+					<h4><?php echo htmlentities($result->RATINGS);?></h4>
+				</div>
+				<br><br>
+			</div>
+
+			
+		<h3>Package Details</h3>
+				<p style="padding-top: 1%"><?php echo htmlentities($result->Restaurant_Details);?> </p>	
+				<div class="clearfix"></div>
+		</div>
+		<div class="selectroom_top">
+			<h2>Restaurant Booking</h2>
+			<div class="selectroom-info animated wow fadeInUp animated" data-wow-duration="1200ms" data-wow-delay="500ms" style="visibility: visible; animation-duration: 1200ms; animation-delay: 500ms; animation-name: fadeInUp; margin-top: -70px">
+				<ul>
+				
+					<li class="spe">
+						<label class="inputLabel">Comment</label>
+						<input class="special" type="text" name="comment" required="">
+					</li>
+					<?php if($_SESSION['login'])
+					{?>
+						<li class="spe"align="center">
+					<button type="submit" name="submit2" class="btn-primary btn">Book</button>
+						</li>
+						<?php } else {?>
+							<li class="sigi" align="center" style="margin-top: 1%">
+							<a href="#" data-toggle="modal" data-target="#myModal4" class="btn-primary btn" > Book</a></li>
+							<?php } ?>
+					<div class="clearfix"></div>
+				</ul>
+			</div>
+			<div class="map-container" style="width: 45%;"><br><br>
+				<div id="map" style="height: 300px; width: 220%; background-color: #f0f0f0;">
+					<tr>
+						<td><?php echo $result->Name; ?></td>
+						<td style="width: 450px; height: 450px;">
+							<iframe src='https://maps.google.com/maps?q=<?php echo $result->Latitude; ?>,<?php echo $result->Longitude; ?>&h1=es;z=14&output=embed' style="width:100%; height:100%">
+							</iframe>
+						</td>
+					</tr>
+			    </div>        
+		    </div>
+		</div>
+		</form>
+<?php }} ?>
+
+
+	</div>
+</div>
+<!--- /selectroom ---->
+<<!--- /footer-top ---->
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>
+</body>
+</html>
---
+++ tsm/package-list-hotel.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TMS  | Package List</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+<!--- banner ---->
+<div class="banner-3">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> TMS- Hotel List</h1>
+	</div>
+</div>
+<!--- /banner ---->
+<!--- rooms ---->
+<div class="rooms">
+	<div class="container">
+		
+		<div class="room-bottom">
+			<h3>Hotel List</h3>
+
+					
+<?php $sql = "SELECT * from hotel";
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>
+			<div class="rom-btm">
+				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
+					<img src="admin/pacakgeimages/<?php echo htmlentities($result->hotel_IMAGE);?>" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
+					<h4>Package Name: <?php echo htmlentities($result->hotel_Name);?></h4>
+					<h6>Package Type : <?php echo htmlentities($result->hotel_PACKAGE_TYPE);?></h6>
+					<p><b>Package Location :</b> <?php echo htmlentities($result->hotel_LOCATION);?></p>
+					<p><b>Features</b> <?php echo htmlentities($result->hotel_FEATURES);?></p>
+				</div>
+				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
+					<h5>PKR <?php echo htmlentities($result->hotel_PRICE_IN_PKR);?></h5>
+					<h5>Rating <?php echo htmlentities($result->hotel_RATINGS);?></h5>
+
+					<a href="package-details-hotel.php?pkgid=<?php echo htmlentities($result->HOTELID );?>" class="view">Details</a>
+					<!-- <a href="?#pkgid=<?php //echo htmlentities($result->HOTELID );?>"class="view">Details</a> -->
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+<?php }} ?>
+			
+		
+		
+		</div>
+	</div>
+</div>
+<!--- /rooms ---->
+
+<!--- /footer-top ---->
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>			
+<!-- //write us -->
+</body>
+</html>
